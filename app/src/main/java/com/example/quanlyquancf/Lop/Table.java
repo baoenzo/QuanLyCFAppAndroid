f6f9d7f6e7e1e105974c155e7885182e848fbc48
Bảo <baoenzo@users.noreply.github.com>
2020-05-27 11:24:09 +0700

    Add table

--- app/src/main/java/com/example/quanlyquancf/Lop/Table.java
+++ app/src/main/java/com/example/quanlyquancf/Lop/Table.java
@@ -1,6 +1,13 @@
 package com.example.quanlyquancf.Lop;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.content.Context;
 import android.os.Bundle;
@@ -10,12 +17,16 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import com.example.quanlyquancf.R;
+import com.google.android.material.navigation.NavigationView;
 
 public class Table extends AppCompatActivity {
 
-    LinearLayout ll;
+    TableLayout tb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,21 +39,57 @@ public class Table extends AppCompatActivity {
 
         LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View view = inflater.inflate(R.layout.activity_table, null);
-        ll = (LinearLayout)findViewById(R.id.ln1);
+        tb = (TableLayout) findViewById(R.id.tb1);
 
-        sett();
+        setTable();
 
     }
-    private void sett()
+    private void setMenu()
     {
-        for(int i =0;i<10;i++) {
+//        final DrawerLayout drawerLayout=(DrawerLayout)findViewById(R.id.drawerlayout);
+//        findViewById(R.id.imageMenu).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                drawerLayout.openDrawer(GravityCompat.START);
+//            }
+//        });
 
+//        NavigationView navigationView=findViewById(R.id.navigationView);
+//        navigationView.setItemIconTintList(null);
+//
+//        NavController navController = Navigation.findNavController(this,R.id.navHostFragment);
+//        NavigationUI.setupWithNavController(navigationView,navController);
+//
+//        final TextView textView=(TextView)findViewById(R.id.textTile);
+//        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+//            @Override
+//            public void onDestinationChanged(@NonNull NavController controller, @NonNull NavDestination destination, @Nullable Bundle arguments) {
+//                textView.setText(destination.getLabel());
+//            }
+//        });
+    }
+    private void setTable()
+    {
+        for(int i = 1;i<=30;i+=2) {
+            int temp = i+1;
             Button myButton = new Button(this);
-            myButton.setText("Push Me " + i);
-            myButton.setWidth(0);
-            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT,0.5f);
-
-            ll.addView(myButton, lp);
+            myButton.setText("Bàn " + i);
+            myButton.setHeight(80);
+            myButton.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_bo_tron_nut_2,0,0,0);
+            Button myButton2 = new Button(this);
+            myButton2.setText("Bàn " +  temp);
+            myButton2.setHeight(80);
+            myButton2.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_bo_tron_nut_2,0,0,0);
+            TableRow.LayoutParams params = new TableRow.LayoutParams(
+                    TableRow.LayoutParams.MATCH_PARENT,
+                    200, 1f);
+            params.setMargins(10,10,10,10);
+            myButton.setLayoutParams(params);
+            myButton2.setLayoutParams(params);
+            TableRow tr1 = new TableRow(this);
+            tr1.addView(myButton, params);
+            tr1.addView(myButton2, params);
+            tb.addView(tr1, new TableLayout.LayoutParams(TableLayout.LayoutParams.FILL_PARENT, TableLayout.LayoutParams.MATCH_PARENT));
         }
     }
 }
